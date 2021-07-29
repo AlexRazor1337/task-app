0258c91111e245d4e073d744eeb83b81f5091988
AlexRazor1337 <alexrazor1337@gmail.com>
2021-07-29 21:19:52 +0300

    Refactor using async/await

--- src/index.js
+++ src/index.js
@@ -11,23 +11,69 @@ const port = process.env.PORT;
 
 app.use(express.json())
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
     const user = new User(req.body);
-    user.save().then(() => {
+
+    try {
+        await user.save();
+        res.send(user);
+    } catch (e) {
         res.status(201).send(user);
-    }).catch((err) => {
-        res.status(400).send(err);
-    });
+    }
 })
 
-app.post('/tasks', (req, res) => {
-    const task = new Task(req.body);
-    task.save().then(() => {
-        res.status(201).send(task);
-    }).catch((err) => {
-        res.status(400).send(err);
-    });
+app.get('/users', async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.send(users);
+    } catch (e) {
+        res.status(500).send();
+    }
 })
+
+
+app.get('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        if (!user) return res.status(404).send();
+
+        res.send(user);
+    } catch (e) {
+        res.status(500).send();
+    }
+})
+
+app.post('/tasks', async (req, res) => {
+    const task = new Task(req.body);
+
+    try {
+        await task.save();
+        res.status(201).send(task);
+    } catch (e) {
+        es.status(400).send(err);
+    }
+})
+
+app.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await Task.find({});
+        res.send(tasks);
+    } catch (e) {
+        res.status(500).send();
+    }
+})
+
+app.get('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findById(req.params.id);
+        if (!task) return res.status(404).send();
+
+        res.send(task);
+    } catch (e) {
+        res.status(500).send();
+    }
+})
+
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port);
