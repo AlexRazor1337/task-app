e9fbebb8952ffafc94dd939f5828b2ab5777b7aa
AlexRazor1337 <alexrazor1337@gmail.com>
2021-08-22 21:48:58 +0300

    sorting, filtering, pagination

--- src/routers/task.js
+++ src/routers/task.js
@@ -18,11 +18,33 @@ router.post('/tasks', auth, async (req, res) => {
     }
 });
 
-
+// completed
+// limit, offset
+// sortBy
 router.get('/tasks', auth, async (req, res) => {
+    const match = {};
+    const sort = {};
+
+    if (req.query.completed) {
+        match.completed = req.query.completed === 'true'
+    }
+
+    if (req.query.sortBy) {
+        const parts = req.query.sortBy.split(':');
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1;
+    }
+
     try {
         const user = req.user;
-        await user.populate('tasks').execPopulate()
+        await user.populate({
+            path: 'tasks',
+            match,
+            options: {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.offset),
+                sort
+            }
+        }).execPopulate();
         res.send(user.tasks);
     } catch (e) {
         res.status(500).send();
